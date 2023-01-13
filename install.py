e8b523bb5e40485da9ab3c98d1e5a8d805643d5b
kex0 <46696708+kex0@users.noreply.github.com>
2023-01-14 00:51:51 +0100

    mediapipe-silicon for apple silicon

--- install.py
+++ install.py
@@ -1,4 +1,10 @@
+import platform
 import launch
 
-if not launch.is_installed("mediapipe"):
-    launch.run_pip("install mediapipe==0.9.0.1", "requirements for Batch Face Swap")
+if not launch.is_installed("mediapipe") or not launch.is_installed("mediapipe-silicon"):
+    name = "Batch Face Swap"
+    if platform.system() == "Darwin":
+        # MacOS
+        launch.run_pip("install mediapipe-silicon", "requirements for Batch Face Swap for MacOS")
+    else:
+        launch.run_pip("install mediapipe", "requirements for Batch Face Swap")
