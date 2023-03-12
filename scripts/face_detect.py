face rotation: refactor rotation calculation into a shared function & fix a minor bug in OpenCV case
The OpenCV path includes code to handle if FaceMesh finds 2+ faces when trying to get
the FaceMesh corresponding to an OpenCV face, by selecting the face that best matches the
OpenCV face. Normally FaceMesh only returns 1 face, but in the case where it found 2+ faces,
the previous version would break. (I think, I don't have any examples that trigger this path.)

scripts/face_detect.py
@@ -110,12 +110,29 @@ def getFacialLandmarks(image, facecfg):
 
         return facelandmarks
 
-#
-# code below here is public domain, cc0, unlicense -- stb
-#
+def computeFaceInfo(landmark, onlyHorizontal, divider, small_width, small_height, small_image_index):
+    x_chin = landmark[152][0]
+    y_chin = -landmark[152][1]
+    x_forehead = landmark[10][0]
+    y_forehead = -landmark[10][1]
+
+    deltaX = x_forehead - x_chin
+    deltaY = y_forehead - y_chin
+    
+    face_angle = math.atan2(deltaY, deltaX) * 180 / math.pi
+
+    # compute center in global coordinates in case the image was split
+    if onlyHorizontal == True:
+        x = ((small_image_index // divider) * small_width ) + landmark[0][0]
+        y = ((small_image_index %  divider) * small_height) + landmark[0][1]
+    else:
+        x = ((small_image_index %  divider) * small_width ) + landmark[0][0]
+        y = ((small_image_index // divider) * small_height) + landmark[0][1]
+
+    return { "angle": face_angle, "center": (x,y) }
 
 # try to get landmarks for a face located at rect
-def getFacialLandmarkConvexHull(image, rect, faces_info, onlyHorizontal, divider, small_width, small_height, small_image_index, facecfg):
+def getFacialLandmarkConvexHull(image, rect, onlyHorizontal, divider, small_width, small_height, small_image_index, facecfg):
     image = np.array(image)
     height, width, _ = image.shape
 
@@ -176,7 +193,9 @@ def getFacialLandmarkConvexHull(image, rect, faces_info, onlyHorizontal, divider
     mp_face_mesh = mp.solutions.face_mesh
 
     best_hull = None
+    best_landmark = None
     best_area = min_match_area
+
     for landmark in landmarks:
         face_info = {}
         convexhull = cv2.convexHull(landmark)
@@ -191,34 +210,17 @@ def getFacialLandmarkConvexHull(image, rect, faces_info, onlyHorizontal, divider
         if area > best_area:
             best_area = area
             best_hull = convexhull
+            best_landmark = landmark
 
-        x_chin = landmark[152][0]
-        y_chin = -landmark[152][1]
-        x_forehead = landmark[10][0]
-        y_forehead = -landmark[10][1]
-
-        deltaX = x_forehead - x_chin
-        deltaY = y_forehead - y_chin
-        
-        face_angle = math.atan2(deltaY, deltaX) * 180 / math.pi
-        if onlyHorizontal == True:
-            x = (small_image_index // (divider) * small_width) + landmark[0][0]
-            y = (small_image_index % (divider) * small_height) + landmark[0][1]
-        else:
-            x = (small_image_index % (divider) * small_width) + landmark[0][0]
-            y = (small_image_index // (divider) * small_height) + landmark[0][1]
-        face_center = (x + subrect_x0, y + subrect_y0)
-        face_info["angle"] = face_angle
-        face_info["center"] = face_center
-        faces_info.append(face_info)
-
+    face_info = None
     if best_hull is not None:
         # translate the convex hull back into the coordinate space of the passed-in image
         for i in range(len(best_hull)):
             best_hull[i][0][0] += subrect_x0
             best_hull[i][0][1] += subrect_y0
+        face_info = computeFaceInfo(best_landmark, onlyHorizontal, divider, small_width, small_height, small_image_index)
 
-    return best_hull, faces_info
+    return best_hull, face_info
 
 def contractRect(r):
     (x0,y0,w,h) = r